72fef90950690f323457dc1edcd3a3ad0d06a464
piotr432 <34043820+piotr432@users.noreply.github.com>
2018-12-07 21:30:49 +0100

    Nowa dzialka

---
+++ NowaDzialka.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t,x;       // liczba testów
+	cin >> t;   // wprowadzanie liczby testow
+
+		for (int i = 1; i <= t; i++)
+		{
+			cin >> x;
+			cout << x*x << endl;
+
+		}
+    return 0;
+}
